bc464ab1eebf4f1e729fe314d13f888007f3b6b0
Ahmed Khaled <hamdawy2012@gmail.com>
2023-02-22 14:55:18 +0100

    repeated String solve

--- src/main/java/org/example/RepeatedString.java
+++ src/main/java/org/example/RepeatedString.java
@@ -1,43 +1,69 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class RepeatedString {
     public static void main(String[] args) {
 
-        long result = repeatedString("aab", 882787L);
+        long result = repeatedString("aba", 10L);
         System.out.println(result);
     }
 
     public static long repeatedString(String s, long n) {
-        try {
+        if (!s.contains("a")) return 0;
 
-            char[] cArr = s.toCharArray();
-            char[] chars = new char[(int) n];
-            int index = 0;
-            for (int i = 0; i <= s.length(); i++) {
-                for (int j = 0; j < s.length(); j++) {
-                    chars[index] = cArr[j];
-                    index++;
-                    if (chars.length == index)
-                        break;
-                }
-            }
-            Map<Character, Integer> result = new TreeMap<>();
-            for (char c : chars) {
-                if (result.containsKey(c)) {
-                    result.replace(c, result.get(c) + 1);
-                } else {
-                    result.put(c, 1);
-                }
-            }
+        if (s.length() == 1 && s.charAt(0) == 'a') return n;
 
-            return result.get(s.charAt(0));
-        } catch (Exception ex) {
-            return n;
+        long counter = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') counter++;
         }
+
+        counter = counter * (n / s.length());
+
+        long remainingCharsCount = n % s.length();
+
+        for (int j = 0; j < remainingCharsCount; j++) {
+            if (s.charAt(j) == 'a') counter++;
+        }
+
+        return counter;
     }
+
+//    public static long repeatedString(String s, long n) {
+//        try {
+//            if (!s.contains("a")) {
+//                return 0;
+//            }
+//            char[] cArr = s.toCharArray();
+//            char[] chars = new char[(int) n];
+//            int index = 0;
+//            for (int i = 0; i <= chars.length; i++) {
+//                for (int j = 0; j < s.length(); j++) {
+//                    chars[index] = cArr[j];
+//                    index++;
+//                    if (n == index) {
+//                        break;
+//                    }
+//                }
+//                if (chars.length == index)
+//                    break;
+//            }
+//            Map<Character, Integer> result = new TreeMap<>();
+//            for (char c : chars) {
+//                if (c == 'a') {
+//                    if (result.containsKey('a')) {
+//                        result.replace('a', result.get('a') + 1);
+//                    } else {
+//                        result.put('a', 1);
+//                    }
+//                }
+//            }
+//
+//            return result.get('a');
+//        } catch (Exception ex) {
+//            return n;
+//        }
+//    }
 }
