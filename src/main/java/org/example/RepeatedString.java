0b0fd72cc594b7750f6b4bcad556a73afedbfa57
Ahmed Khaled <hamdawy2012@gmail.com>
2023-02-22 14:55:38 +0100

    remove commented code

--- src/main/java/org/example/RepeatedString.java
+++ src/main/java/org/example/RepeatedString.java
@@ -31,39 +31,4 @@ public class RepeatedString {
         return counter;
     }
 
-//    public static long repeatedString(String s, long n) {
-//        try {
-//            if (!s.contains("a")) {
-//                return 0;
-//            }
-//            char[] cArr = s.toCharArray();
-//            char[] chars = new char[(int) n];
-//            int index = 0;
-//            for (int i = 0; i <= chars.length; i++) {
-//                for (int j = 0; j < s.length(); j++) {
-//                    chars[index] = cArr[j];
-//                    index++;
-//                    if (n == index) {
-//                        break;
-//                    }
-//                }
-//                if (chars.length == index)
-//                    break;
-//            }
-//            Map<Character, Integer> result = new TreeMap<>();
-//            for (char c : chars) {
-//                if (c == 'a') {
-//                    if (result.containsKey('a')) {
-//                        result.replace('a', result.get('a') + 1);
-//                    } else {
-//                        result.put('a', 1);
-//                    }
-//                }
-//            }
-//
-//            return result.get('a');
-//        } catch (Exception ex) {
-//            return n;
-//        }
-//    }
 }
